0806733762f583208c24f7ab0c9011ddccfc1c7b
Tim Welch <tim.j.welch@gmail.com>
2015-10-17 17:49:46 -0700

    working with react 0.14 and gl-js 0.11.1

---
+++ App.js
@@ -0,0 +1,60 @@
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      mapToken: 'insert your public token here',
+      mapView: {
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v8',
+        center: [-122.6, 45.5],
+        zoom: 9
+      }
+    };
+  },
+
+  render: function() {
+    return (
+      <div className="App">
+        <GLMap view={this.state.mapView} token={this.state.mapToken}/>
+      </div>
+    )
+  }
+});
+
+var GLMap = React.createClass({
+  propTypes: {
+    view: React.PropTypes.object,
+    token: React.PropTypes.string
+  },
+
+  render: function() {
+    var mapStyle = {
+      position: 'absolute',
+      top:0,
+      bottom:0,
+      width:'100%'
+    };
+
+    return (
+      <div>
+        <div id='map' style={mapStyle}></div>
+      </div>
+    )
+  },
+
+  componentDidMount: function() {
+    var el = ReactDOM.findDOMNode();
+    mapboxgl.accessToken = this.props.token;
+    this.map = new mapboxgl.Map(this.props.view);
+  },
+
+  componentWillUnmount: function() {
+    this.map.remove();
+    ReactDOM.unmountComponentAtNode(ReactDOM.findDOMNode());
+  }
+});
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('content')
+);
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-mapgbox-gl
-Mapbox-gl-js react component test
+Simple React component for a Mapbox GL (aka mapbox-gl-js) map
+
+A Mapbox GL map creates and manages its own DOM elements outside of React's virtual DOM.  Not a problem, we can still encapsulate a map into a React component and keep things separate. This example borrows heavily from a [blog post](http://nicolashery.com/integrating-d3js-visualizations-in-a-react-app/) written by Nicolas Hery demonstrating a technique for incorporating D3 into a React app.
